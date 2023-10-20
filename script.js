8a1054a8c6bd39349c0258cc1a94a0e72c83df2b
CamposJayme <108497638+CamposJayme@users.noreply.github.com>
2023-10-19 23:14:21 -0300

    tab navigator with content

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 const menu = document.querySelectorAll('#menu li')
 
-menu.forEach((item) => {
-    item.addEventListener('mouseover', function() {
-        item.classList.add('ativo')
+function activeMenu() {
+    menu.forEach((item) => {
+        item.addEventListener('mouseover', function() {
+            item.classList.add('ativo')
+        })
     })
-})
+}
+activeMenu()
 
+const sectionMenu = document.querySelectorAll('.conteudo div img')
+const section = document.querySelectorAll('.conteudo')
+
+function activeContent() {
+    if (sectionMenu.length && section.length) {
+        function activeTab(index) {
+            section.forEach(() => {
+                section[index].classList.toggle('texto')
+            })
+        }
+        
+        sectionMenu.forEach((imgItem, index) => {
+            imgItem.addEventListener('click', () => {
+                activeTab(index)
+            })
+        })
+    }
+}
+activeContent()
