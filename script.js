a50a8dda1a6014ea1cf400e10542ab4bc7c9b51d
CamposJayme <108497638+CamposJayme@users.noreply.github.com>
2023-10-21 14:37:47 -0300

    menu animations removed

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-const menu = document.querySelectorAll('#menu li')
-
-function activeMenu() {
-    menu.forEach((item) => {
-        item.addEventListener('mouseover', function() {
-            item.classList.add('ativo')
-        })
-    })
-}
-activeMenu()
-
 const sectionMenu = document.querySelectorAll('.conteudo div img')
 const section = document.querySelectorAll('.conteudo')
 
